9b080d23c500d159d5da88cd248850ef4ac85afb
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-09-20 16:26:58 +0300

    ПР15 диплой

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://yva.mesto.nomoredomains.sbs',
     credentials: true,
   }),
 );
