5d362ea0135fa5de030d2a0cf3f0a7875ca5e947
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-09-21 21:24:45 +0300

    ПР15 диплой

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,9 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Сначала нужно авторизироваться');
+  }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
